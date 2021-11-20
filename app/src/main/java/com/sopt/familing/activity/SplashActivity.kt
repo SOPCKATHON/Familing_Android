97b54a0df03312624eecb38a05e19e1e0d28f7f9
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-21 01:16:31 +0900

    [FEAT] Add HomeActivity.kt

--- app/src/main/java/com/sopt/familing/activity/SplashActivity.kt
+++ app/src/main/java/com/sopt/familing/activity/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent= Intent(this,StampActivity::class.java)
+            val intent= Intent(this,HomeActivity::class.java)
             startActivity(intent)
         },100) //1초가 1000millis
         //TODO : 나중에 초 변경
