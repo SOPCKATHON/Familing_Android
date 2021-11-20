bc6e5e0f2e66d5d98f44ff664ea1d16e982f338b
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-21 07:33:23 +0900

    [FEAT] Change Month by Arrow button

--- app/src/main/java/com/sopt/familing/fragment/StampFragment.kt
+++ app/src/main/java/com/sopt/familing/fragment/StampFragment.kt
@@ -15,17 +15,47 @@ class StampFragment : Fragment() {
 
     private var _binding: FragmentStampBinding?=null
     private val binding get()=_binding!!
+    var curr_month = 11
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding= FragmentStampBinding.inflate(layoutInflater,container,false)
+
         initFamilyName()
+        initNetwork()
+        initListener()
+
 
         return binding.root
     }
 
+    private fun initNetwork(){
+        // 11월 관련 통신 하기
+
+    }
+
+
+    private fun initListener(){
+        binding.ivPrevMonth.setOnClickListener{
+            // 지난달 데이터 받기
+            Log.d("prevMonth", "prevMonth")
+            curr_month -= 1
+            if (curr_month < 1) curr_month = 12
+            val month = curr_month.toString() + "월"
+            binding.tvMonth.text = month
+        }
+        binding.ivNextMonth.setOnClickListener{
+            // 다음달 데이터 받기
+            Log.d("nextMonth", "nextMonth")
+            curr_month += 1
+            if (curr_month > 12) curr_month = 1
+            val month = curr_month.toString() + "월"
+            binding.tvMonth.text = month
+
+        }
+    }
 
     private fun initFamilyName(){
 
