007fee7eec202242958713bffcc3697457e137f7
hyunji <24hyunji@naver.com>
2021-11-21 03:27:31 +0900

    [FEAT] OnBoarding modified

--- app/src/main/java/com/sopt/familing/fragment/MyPageFragment.kt
+++ app/src/main/java/com/sopt/familing/fragment/MyPageFragment.kt
@@ -13,7 +13,7 @@ class MyPageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState:Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
