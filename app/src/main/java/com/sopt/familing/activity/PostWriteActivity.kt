e582023490ad2ab01167bfd47059512bfdadc025
minwoo <alsd20177@gmail.com>
2021-11-21 05:45:29 +0900

    [UI] Edit layout

--- app/src/main/java/com/sopt/familing/activity/PostWriteActivity.kt
+++ app/src/main/java/com/sopt/familing/activity/PostWriteActivity.kt
@@ -2,17 +2,16 @@ package com.sopt.familing.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sopt.familing.R
 import com.sopt.familing.databinding.ActivityPostWriteBinding
 
 class PostWriteActivity : AppCompatActivity() {
-    private var isChipSelected : Int = -1
+    private var whatChipSelected : Int = -1
     private lateinit var binding : ActivityPostWriteBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityPostWriteBinding.inflate(layoutInflater)
+        initChipSListener()
         setContentView(binding.root)
     }
 
@@ -21,45 +20,80 @@ class PostWriteActivity : AppCompatActivity() {
     private fun initChipSListener(){
         // 외식
         binding.chip0.setOnClickListener {
-            binding.chipGroup.clearCheck()
-            binding.chip0.isChecked = true
-            isChipSelected = 0
-
+            if(whatChipSelected == 0){
+                binding.chip0.isSelected = false
+                whatChipSelected = -1
+            }
+            else{
+                binding.chip0.isSelected = true
+                binding.chip1.isSelected = false
+                binding.chip2.isSelected = false
+                binding.chip3.isSelected = false
+                binding.chip4.isSelected = false
+                whatChipSelected = 0
+            }
         }
 
         binding.chip1.setOnClickListener {
-            binding.chipGroup.clearCheck()
-            binding.chip1.isChecked = true
-            isChipSelected = 1
+            if(whatChipSelected == 1){
+                binding.chip1.isSelected = false
+                whatChipSelected = -1
+            }
+            else {
+                binding.chip0.isSelected = false
+                binding.chip1.isSelected = true
+                binding.chip2.isSelected = false
+                binding.chip3.isSelected = false
+                binding.chip4.isSelected = false
+                whatChipSelected = 1
+            }
         }
 
         binding.chip2.setOnClickListener {
-            binding.chipGroup.clearCheck()
-            binding.chip2.isChecked = true
-            isChipSelected = 2
+            if(whatChipSelected == 2){
+                binding.chip2.isSelected = false
+                whatChipSelected = -1
+            }
+            else{
+                binding.chip0.isSelected = false
+                binding.chip1.isSelected = false
+                binding.chip2.isSelected = true
+                binding.chip3.isSelected = false
+                binding.chip4.isSelected = false
+                whatChipSelected = 2
+            }
         }
 
         binding.chip3.setOnClickListener {
-            binding.chipGroup.clearCheck()
-            binding.chip3.isChecked = true
-            isChipSelected = 3
+            if(whatChipSelected == 3){
+                binding.chip3.isSelected = false
+                whatChipSelected = -1
+            }
+            else {
+                binding.chip0.isSelected = false
+                binding.chip1.isSelected = false
+                binding.chip2.isSelected = false
+                binding.chip3.isSelected = true
+                binding.chip4.isSelected = false
+                whatChipSelected = 3
+            }
         }
 
         binding.chip4.setOnClickListener {
-            binding.chipGroup.clearCheck()
-            binding.chip3.isChecked = true
-            isChipSelected = 3
+            if(whatChipSelected == 4){
+                binding.chip4.isSelected = false
+                whatChipSelected = -1
+            }
+            else{
+                binding.chip0.isSelected = false
+                binding.chip1.isSelected = false
+                binding.chip2.isSelected = false
+                binding.chip3.isSelected = false
+                binding.chip4.isSelected = true
+                whatChipSelected = 4
+            }
         }
     }
-
-    // 등록 버튼을 누르면 chip group 중 몇번 chip이 선택되었는지에 따라 서버에 정보 전달할 때 카테고리 정수값 전달
-    /*
-    View.OnClickListener {
-            binding.cgChip.clearCheck()
-            chip.isChecked = true
-        }
-     */
-
 
 
     companion object{
