b2a27b4bd6fd37a36cdfe09fd8b17c1e851485c5
KxxHyoRim <rlagyfla0709@gmail.com>
2021-11-21 00:10:34 +0900

    [FEAT] Add Splash

---
+++ app/src/main/java/com/sopt/familing/activity/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.sopt.familing.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.sopt.familing.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent= Intent(this,StampActivity::class.java)
+            startActivity(intent)
+        },100) //1초가 1000millis
+        //TODO : 나중에 초 변경
+    }
+}
